e77e427faa240a63a9a46d0cc4f640e34d307f54
roaucar <132701908+roaucar@users.noreply.github.com>
2024-06-30 21:08:52 -0300

    Update consultar_ranking_paises.php

--- backend/static/php/consultar_ranking_paises.php
+++ backend/static/php/consultar_ranking_paises.php
@@ -1,5 +1,4 @@
 <?php
-// Aquí debes reemplazar con la lógica adecuada para consultar el ranking de países desde tu base de datos
-// Supongamos que tienes una tabla 'ranking_paises' con campos 'name' y 'rating'
+//  para consultar el ranking de países desde tu base de datos
 
 ?>
