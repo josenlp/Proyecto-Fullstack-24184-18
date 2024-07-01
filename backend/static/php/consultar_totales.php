a432359ed94ca0060f0d05f97929860df108fbdb
roaucar <132701908+roaucar@users.noreply.github.com>
2024-06-30 21:09:22 -0300

    Update consultar_totales.php

--- backend/static/php/consultar_totales.php
+++ backend/static/php/consultar_totales.php
@@ -1,5 +1,7 @@
 <?php
-// Aquí debes reemplazar con la lógica adecuada para consultar los totales desde tu base de datos
-// Supongamos que tienes una tabla 'totales' con campos 'totalConsultas', 'totalMasculino', 'totalFemenino', 'totalOtro'
+// para consultar los totales desde tu base de datos
+?>
+
+
 
 
